7e5f7c24b5f09a2549e939fee5147ebc6b69c1f1
Andrew Tonner <rakslice@gmail.com>
2016-12-03 00:33:47 -0800

    comment out the bindings for the missing notification message events on unix for now

--- src/twitchnotifier/main_unix.go
+++ src/twitchnotifier/main_unix.go
@@ -6,8 +6,9 @@ package main
 import "github.com/dontpanic92/wxGo/wx"
 
 func (win *MainStatusWindowImpl) additionalBindings() {
-	wx.Bind(win.toolbar_icon, wx.EVT_NOTIFICATION_MESSAGE_CLICK, win._on_toolbar_balloon_click, wx.ID_ANY)
-	wx.Bind(win.toolbar_icon, wx.EVT_NOTIFICATION_MESSAGE_DISMISSED, win._on_toolbar_balloon_timeout, wx.ID_ANY)
+	// FIXME the event constants for these appear to be missing
+	//wx.Bind(win.toolbar_icon, wx.EVT_NOTIFICATION_MESSAGE_CLICK, win._on_toolbar_balloon_click, wx.ID_ANY)
+	//wx.Bind(win.toolbar_icon, wx.EVT_NOTIFICATION_MESSAGE_DISMISSED, win._on_toolbar_balloon_timeout, wx.ID_ANY)
 }
 
 func (win *MainStatusWindowImpl) osNotification(notification *NotificationQueueEntry) {
