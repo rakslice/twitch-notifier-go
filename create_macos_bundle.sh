3c2275876e560da181ba23aefd935ffbc7d28e10
rakslice <rakslice@gmail.com>
2016-12-04 20:27:52 -0800

    bundle script and Info.plist for macOS

---
+++ create_macos_bundle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e -x
+
+# set up paths
+mkdir -p twitch-notifier-go.app/Contents/MacOS
+mkdir -p twitch-notifier-go.app/Contents/Resources
+mkdir -p twitch-notifier-go.app/Contents/Resources/English.lproj
+
+# binary
+cp ../../../../bin/twitchnotifier twitch-notifier-go.app/Contents/MacOS/
+SetFile -t APPL twitch-notifier-go.app/Contents/MacOS/twitchnotifier
+
+# info.plist
+cp Info.plist twitch-notifier-go.app/Contents/
+
+# PkgInfo
+echo -n 'APPL????' > twitch-notifier-go.app/Contents/PkgInfo
+
+# Icon
+# - convert icon to icns
+sips -s format icns src/twitchnotifier/assets/icon.ico --out twitch-notifier-go.app/Contents/Resources/icon.icns
+
+
